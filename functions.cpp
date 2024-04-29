72c498ccb41e63e84a3ba8bc4fbea290475e1313
Luke Teran <luketeran@tamu.edu>
2024-04-29 09:50:45 -0500

    Cleaned up comments

--- functions.cpp
+++ functions.cpp
@@ -8,8 +8,6 @@
 #include "header.h"
 // Define any global constants or macros here
 extern const int SAMPLE_SIZE;
-// Declare any global variables here
-// Define any global functions or classes here
 // Implementation of functions or classes, if applicable
 
 signed int fn_a(int a, int b) {
